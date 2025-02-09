cacae8967aaca9abe8bb47bc3a2f920c1fe9bd59
MartinGotelli <martingotelliferenaz@gmail.com>
2025-02-09 20:32:19 -0300

    fix: Fix plugin installation in the conftest

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -5,10 +5,6 @@ from pytest_matchers import plugin
 os.environ["PYTEST_MATCHERS_WARNINGS"] = "False"
 
 
-def pytest_configure(config):
-    config.pluginmanager.register(plugin)
-
-
 class CustomEqual:
     def __init__(self, value):
         self.value = value
